3be01bc4938768ee2f5de6e6e8a44ae713d2dc81
JorshGL <jorge.gl0905@gmail.com>
2023-03-01 16:48:40 -0500

    a

--- src/components/NavigatorButton.jsx
+++ src/components/NavigatorButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const NavigatorButton = ({text, route}) => {
+const NavigatorButton = ({ text, route }) => {
   const navigate = useNavigate();
+
   return (
     <button
       className="p-4 rounded-md bg-green-600 text-xl font-bold"
